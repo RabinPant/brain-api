bdc43bf53153b0c5116abf14d99b0d49f074e14c
RabinPant <pantrabin12@gmail.com>
2020-07-12 14:43:36 -0700

    adding port

--- server.js
+++ server.js
@@ -117,6 +117,6 @@ app.put('/image',(req,res)=>{
     })
     .catch(err=>res.status(400).json('unable to get entries'))
 })
-app.listen(3001,()=>{
-    console.log('app is running in port 3001');
+app.listen(process.env.PORT|| 3000,()=>{
+    console.log(`app is running in port ${process.env.PORT}`);
 })
